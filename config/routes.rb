af3a22b5af9d012966f2c005add4cf90d2691654
samurai-sa <football03xtk@gmail.com>
2021-08-03 17:23:28 +0900

    :fire: 不要なスペース削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  
+
   root to: 'public/homes#top'
   devise_for :admins
   devise_for :end_users
-  
+
   namespace :admin do
     get '/' => 'homes#top'
     get 'end_users/index'
-    
+
     resources :items, only: [:index, :show, :new, :create, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :orders, only: [:index, :show, :update]
     resources :order_details, only: [:update]
   end
-  
+
   scope module: 'public' do
     get '/about' => 'homes#about'
     get 'customers/show'
@@ -25,12 +25,11 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :show]
     resources :cart_items, only: [:index, :create, :update, :destroy]
     delete 'cart_items' => 'cart_items#destroy_all', as:'destroy_all'
-    
+
     get 'orders/done' => 'orders#done'
     get 'orders/confirm'
     resources :orders, only: [:index, :new, :create, :show]
-    
+
     resources :addresses
   end
-  
 end

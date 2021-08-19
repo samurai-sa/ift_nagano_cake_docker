5aec3a48ccee035e6439205bc07f8a83b2c07c5e
samurai-sa <football03xtk@gmail.com>
2021-08-20 00:49:41 +0900

    :pencil2: seedデータを書き換える

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,21 @@ Admin.create!(
   password: "admin01"
 )
 
+# 会員ダミーデータ
+10.times do |n|
+  EndUser.create!(
+    email: "test#{n + 1}@example.com",
+    password: "test0#{n + 1}",
+    last_name: "テスト#{n + 1}",
+    first_name: "太郎#{n + 1}",
+    last_name_kana: "テスト#{n + 1}",
+    first_name_kana: "タロウ#{n + 1}",
+    postal_code: sprintf('%06d', rand(999999)),
+    address: "テスト県ダミー区#{n + 1}",
+    telephone_number: sprintf('%011d', rand(99999999999))
+  )
+end
+
 # ジャンルダミーデータ
 Genre.create!(
   [
@@ -19,17 +34,25 @@ Genre.create!(
       name: "和菓子"
     },
     {
-      name: "洋菓子"
+      name: "ケーキ"
+    },
+    {
+      name: "クッキー"
+    },
+    {
+      name: "チョコレート"
     }
   ]
 )
 
 # 商品ダミーデータ
-Item.create!(
-  genre_id: 1,
-  name: "ケーキ",
-  image_id: "",
-  introduction: "新作ケーキです。",
-  price: 1000,
-  is_active: true
-)
+5.times do |n|
+  Item.create!(
+    genre_id: rand(1..4),
+    name: "ケーキ#{n + 1}",
+    image_id: "",
+    introduction: "期間限定商品です",
+    price: rand(1000..5000),
+    is_active: true
+  )
+end

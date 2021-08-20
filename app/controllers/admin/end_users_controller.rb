394fd843b96a4db591461177e32a5d9c92524c61
samurai-sa <football03xtk@gmail.com>
2021-08-20 22:13:00 +0900

    :sparkles: 会員詳細ページ作成

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -4,4 +4,10 @@ class Admin::EndUsersController < ApplicationController
     @end_users = EndUser.all
   end
 
+  def show
+    @end_user = EndUser.find(params[:id])
+    @orders = @end_user.orders
+    @addresses = @end_user.addresses
+  end
+
 end
